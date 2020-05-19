41ef5c52b2ea97b4deefbabb50165a782732b2ea
Mikedu1988 <duliyang@juzhou.io>
2020-05-19 19:17:16 +0800

    add config

--- image/skopeo-run.sh
+++ image/skopeo-run.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 strB="\|"
-while true
-do
-#  echo "download image list"
-#  rm imagelist
-  cat imagelist| while read LINE
-  do
+sourceImage=""
+targetImage=""
+while true; do
+  #  echo "download image list"
+  #  rm imagelist
+  cat imagelist | while read LINE; do
     echo ${LINE}
-    if [[ ${LINE} =~ ${strB} ]]
-    then
-      sourceImage=`echo ${LINE} | awk 'BEGIN {FS="|";} { print $1 }'`
-      targetImage=`echo ${LINE} | awk 'BEGIN {FS="|";} { print $2 }'`
+    if [[ ${LINE} =~ ${strB} ]]; then
+      sourceImage=$(echo ${LINE} | awk 'BEGIN {FS="|";} { print $1 }')
+      targetImage=$(echo ${LINE} | awk 'BEGIN {FS="|";} { print $2 }')
+    else
+      sourceImage=${LINE}
+      targetImage=${LINE}
+    fi
+    skopeo inspect docker-daemon:${targetImage} >> /dev/null
+    if [[ $? -eq 0 ]]; then
+      echo "image exists on node, do nothing"
+    else
       echo "run command skopeo copy docker://${sourceImage} docker-daemon:${targetImage}"
       skopeo copy docker://${sourceImage} docker-daemon:${targetImage}
-    else
-      echo "run command skopeo copy docker://${LINE} docker-daemon:${LINE}"
-      skopeo copy docker://${LINE} docker-daemon:${LINE}
     fi
   done
   echo "sleep for 60 seconds"
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 a k8s daemonset to pull node image
+
+running a daemonset on each node to pull images for new nodes.
+for image registries like quay.io,gcr.io, we can pull and push the image from our mac and push to a ecr or docker io.
